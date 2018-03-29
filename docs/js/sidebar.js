d1d059690c37fd69877ce1c0452d265973e8408c
Ingy döt Net <ingy@ingy.net>
2018-03-29 13:53:28 -0700

    ...

--- docs/js/sidebar.js
+++ docs/js/sidebar.js
@@ -1,1 +1,11 @@
-../../../aaa/site/js/sidebar.js
+$(function() {
+    $("li")
+        .mouseenter(function() {
+            $(this).find('ul').show(300);
+        });
+    $(".sidebar")
+        .mouseleave(function() {
+            $(this).find('li ul').hide(500);
+        });
+});
+
